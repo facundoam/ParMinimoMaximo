9c5f7e5c5da2709ececd862924f377cda824de14
Facundo Arias <farias@facunotebook.argentina.ibm.com>
2016-03-30 08:26:08 -0300

    ParMinimoMaximo Java - Programación III

--- src/ejecutable.java
+++ src/ejecutable.java
@@ -1,5 +1,5 @@
 /**
- * Created by farias on 23/03/16.
+ * @author farias on 23/03/16.
  */
 public class ejecutable {
 
@@ -9,7 +9,6 @@ public class ejecutable {
         int arreglo[] = {2, 3, 1, 12, 34,151, 56, 78, 65, 10, 91,14};
 
         ParMinClase A = new ParMinClase(arreglo);
-
 
         System.out.println(A.getMax());
         System.out.println(A.getMin());
